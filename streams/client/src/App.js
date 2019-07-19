2e3f8eb6f0288b7ab2d0f407239b79f56d72522a
blaise <ffringel9@gmail.com>
2019-07-19 13:59:34 -0500

    Refactor StreamCreate & StreamEdit

--- streams/client/src/App.js
+++ streams/client/src/App.js
@@ -1,25 +1,26 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 import Header from "./components/Header";
 import StreamCreate from "./components/streams/StreamCreate";
 import StreamDelete from "./components/streams/StreamDelete";
 import StreamEdit from "./components/streams/StreamEdit";
 import StreamList from "./components/streams/StreamList";
 import StreamShow from "./components/streams/StreamShow";
+import history from "./history";
 
 function App() {
   return (
     <div className="ui container">
-      <BrowserRouter>
+      <Router history={history}>
         <div>
           <Header />
           <Route path="/" exact component={StreamList} />
           <Route path="/streams/new" exact component={StreamCreate} />
-          <Route path="/streams/edit" exact component={StreamEdit} />
-          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/edit/:id" exact component={StreamEdit} />
+          <Route path="/streams/delete/:id" exact component={StreamDelete} />
           <Route path="/streams/show" exact component={StreamShow} />
         </div>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 }
